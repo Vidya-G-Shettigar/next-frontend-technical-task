d3e04b4a353b1a0cf4cbab61dbf02842fab35fa4
Vidya-G-Shettigar <61654722+Vidya-G-Shettigar@users.noreply.github.com>
2022-12-23 20:28:17 +0530

    Added files to src folder

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import DisplayMusicData from './components/DisplayMusicData';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayMusicData/>
+    </div>
+  );
+}
+
+export default App;
